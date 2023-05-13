782c16a6d852185510f700cb676dcbe035283714
000Lucifer <gabrielpaez0000@gmail.com>
2023-05-13 18:22:22 -0300

    nose que estoi asiendo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Inicio from "./pages/Inicio"
-import Servicios from "./pages/Servicios"
-import SobreNosotros from "./pages/SobreNosotros"
-import Contactenos from "./pages/Contactenos"
+// import Servicios from "./pages/Servicios"
+// import SobreNosotros from "./pages/SobreNosotros"
+// import Contactenos from "./pages/Contactenos"
 const App = () => {
-  return <Contactenos />
+  return <Inicio />
 }
 
 export default App
--- src/components/globales/Navbar.jsx
+++ src/components/globales/Navbar.jsx
@@ -4,12 +4,18 @@ const Navbar = () => {
     <div className={styles.navbars}>
       <img className={styles.iconos} alt='' src='/iconos4.svg' />
       <div className={styles.empresa}>Empresa</div>
-      <div className={styles.navbarLinks}>
+      <ul className={styles.navbarLinks}>
+        <li className={styles.linkButtons2}><div className={styles.home}>Home</div></li>
+        <li className={styles.linkButtons2}><div className={styles.home}>Nosotros</div></li>
+        <li className={styles.linkButtons2}><div className={styles.home}>Servicios</div></li>
+        <li className={styles.linkButtons2}><div className={styles.home}>Contacto</div></li>
+      </ul>
+      {/* <div className={styles.navbarLinks}>
         <div className={styles.linkButtons2}>
-          <div className={styles.home}>Servicios</div>
+          <div className={styles.home}>Home</div>
         </div>
         <div className={styles.linkButtons2}>
-          <div className={styles.home}>Servicios</div>
+          <div className={styles.home}>Nosotros</div>
         </div>
         <div className={styles.linkButtons2}>
           <div className={styles.home}>Servicios</div>
@@ -17,7 +23,7 @@ const Navbar = () => {
         <div className={styles.linkButtons2}>
           <div className={styles.home}>Contacto</div>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
