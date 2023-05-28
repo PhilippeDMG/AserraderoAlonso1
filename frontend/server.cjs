ed5548e756ab76c384ec8c68ca94e50e918a9922
000Lucifer <gabrielpaez0000@gmail.com>
2023-05-28 20:19:50 -0300

    sigo acomodando webadas

--- frontend/server.cjs
+++ frontend/server.cjs
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 // Servir archivos estáticos desde el directorio 'assets'
-app.use('/assets', express.static(path.join(__dirname + './dist/assets/'), {
+app.use('/assets', express.static(path.join(__dirname + '/dist/assets/'), {
   setHeaders: (res, path) => {
     if (path.endsWith('.css')) {
       res.setHeader('Content-Type', 'text/css');
@@ -12,29 +12,29 @@ app.use('/assets', express.static(path.join(__dirname + './dist/assets/'), {
 }));
 
 // Servir archivos estáticos desde los directorios de cada página
-app.use(express.static(__dirname + './dist/src/pages/inicio/'));
-app.use('/contacto', express.static(path.join(__dirname + './dist/src/pages/contactenos/')));
-app.use('/servicios', express.static(path.join(__dirname + './dist/src/pages/servicios/')));
-app.use('/nosotros', express.static(path.join(__dirname + './dist/src/pages/sobreNosotros/')));
+app.use(express.static(__dirname + '/dist/src/pages/inicio/'));
+app.use('/contacto', express.static(path.join(__dirname + '/dist/src/pages/contactenos/')));
+app.use('/servicios', express.static(path.join(__dirname + '/dist/src/pages/servicios/')));
+app.use('/nosotros', express.static(path.join(__dirname + '/dist/src/pages/sobreNosotros/')));
 
 // Servir archivos estáticos desde el directorio raíz
-app.use(express.static(path.join(__dirname + './dist/')));
+app.use(express.static(path.join(__dirname + '/dist/')));
 
 // Rutas para cada página
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + './dist/src/pages/inicio/index.html')) 
+  res.sendFile(path.join(__dirname + '/dist/src/pages/inicio/index.html')) 
 })
 
 app.get('/contacto', (req, res) => {
-  res.sendFile(path.join(__dirname + './dist/src/pages/contactenos/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/src/pages/contactenos/index.html'))
 }) 
 
 app.get('/servicios', (req, res) => {
-  res.sendFile(path.join(__dirname + './dist/src/pages/servicios/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/src/pages/servicios/index.html'))
 })  
 
 app.get('/nosotros', (req, res) => {
-  res.sendFile(path.join(__dirname + './dist/src/pages/sobreNosotros/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/src/pages/sobreNosotros/index.html'))
 })
 
 app.listen(3000, () => {
