7111773eedee2c019b6bfe1f3b047b9e14637167
PhilippeDMG <philippedamau@gmail.com>
2023-05-29 12:43:10 -0300

    miscontacts

--- frontend/src/components/globales/Navbar.jsx
+++ frontend/src/components/globales/Navbar.jsx
@@ -1,31 +1,51 @@
 import style from "./Navbar_.module.css"
-import {useScreenSize} from '../../utils/useScreenSize'
+import { useScreenSize } from "../../utils/useScreenSize"
+import Desplegable from "./Desplegable"
+import { useState } from "react"
+
 const Navbar = () => {
-  let body = useScreenSize(
-    {
-      small: 'bodySmall',
-      medium: 'bodyMedium',
-      large: 'bodyLarge'
-    }
-  )
-    
-  let label = useScreenSize(
-    {
-      small: 'labelSmall',
-      medium: 'labelMedium',
-      large: 'labelLarge'
-    }
-  )
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisible = () => {
+    setVisible(!visible)
+  }
+  let body = useScreenSize({
+    small: "bodySmall",
+    medium: "bodyMedium",
+    large: "bodyLarge",
+  })
+
+  let label = useScreenSize({
+    small: "labelSmall",
+    medium: "labelMedium",
+    large: "labelLarge",
+  })
   return (
     <div className={style.navbar}>
       {/* <img className={style.icono} alt='' src='/home.svg' /> */}
-      <div className={body}><span>Alonso</span> TimberCraft</div>
-      <img className={style.burger} alt='' src='/burger.svg'></img>
+      <div className={body}>
+        <span>Alonso</span> TimberCraft
+      </div>
+      <Desplegable visible={visible} />
+      <img
+        className={style.burger}
+        alt=''
+        src='/burger.svg'
+        onClick={() => toggleVisible()}
+      />
       <ul className={style.navbarLinks}>
-        <li className={`${style.linkButtons} ${label}`}><a href="/">Inicio</a></li>
-        <li className={`${style.linkButtons} ${label}`}><a href="/nosotros">Nosotros</a></li>
-        <li className={`${style.linkButtons} ${label}`}><a href="/servicios">Servicios</a></li>
-        <li className={`${style.linkButtons} ${label}`}><a href="/contacto">Contacto</a></li>
+        <li className={`${style.linkButtons} ${label}`}>
+          <a href='/'>Inicio</a>
+        </li>
+        <li className={`${style.linkButtons} ${label}`}>
+          <a href='/nosotros'>Nosotros</a>
+        </li>
+        <li className={`${style.linkButtons} ${label}`}>
+          <a href='/servicios'>Servicios</a>
+        </li>
+        <li className={`${style.linkButtons} ${label}`}>
+          <a href='/contacto'>Contacto</a>
+        </li>
       </ul>
     </div>
   )
