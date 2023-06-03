ed01539c2ebca0320709b2a102ce989e55a1016a
000Lucifer <gabrielpaez0000@gmail.com>
2023-06-03 02:04:38 -0300

    email funcionando con animacion simple en form

--- back/server.js
+++ back/server.js
@@ -1,6 +1,11 @@
 /* eslint-disable no-undef */
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+
+app.use(cors());
+app.use(express.json());
 // Servir archivos estáticos desde el directorio 'assets'
 app.use('/assets', express.static(__dirname + '/../frontend/dist/assets/', {
   setHeaders: (res, path) => {
@@ -9,15 +14,46 @@ app.use('/assets', express.static(__dirname + '/../frontend/dist/assets/', {
     }
   }
 }));
-
 // Servir archivos estáticos desde los directorios de cada página
 app.use(express.static(__dirname + '/../frontend/dist/src/pages/inicio/'));
 app.use('/contacto', express.static(__dirname + '/../frontend/dist/src/pages/contactenos/'));
 app.use('/servicios', express.static(__dirname + '/../frontend/dist/src/pages/servicios/'));
 app.use('/nosotros', express.static(__dirname + '/../frontend/dist/src/pages/sobreNosotros/'));
-
 // Servir archivos estáticos desde el directorio raíz
 app.use(express.static(__dirname + '/../frontend/dist/'));
+
+app.post('/send-email', (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'manuelnoru@gmail.com', // Cambiar con la dirección de correo electrónico del remitente
+      pass: 'rvor wquk dcay rdez' // Cambiar con la contraseña del correo electrónico del remitente
+    }
+  });
+
+  const mailOptions = {
+    from: 'manuelnoru@gmail.com', // Cambiar con la dirección de correo electrónico del remitente
+    to: 'manuelnoru@gmail.com', // Cambiar con la dirección de correo electrónico del destinatario
+    subject: 'New message from your website',
+    html: `
+      <p>Name: ${name}</p>
+      <p>Email: ${email}</p>
+      <p>Message: ${message}</p>
+    `
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
 
 // Rutas para cada página
 app.get('/', (req, res) => {
--- frontend/src/components/globales/ContactForm.jsx
+++ frontend/src/components/globales/ContactForm.jsx
@@ -1,19 +1,47 @@
+import { useState } from 'react';
 import styles from './ContactForm.module.css';
 import { useScreenSize } from '../../utils/useScreenSize';
+import axios from 'axios';
 
 const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [status, setStatus] = useState('');
+  const [statusStyle, setStatusStyle] = useState(styles.statusSuccess);
+  const [timeoutId, setTimeoutId] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('/send-email', { name, email, message })
+      .then(response => {
+        console.log(response.data);
+        setStatus('Email sent successfully');
+        setMessage(''); setName(''); setEmail('');
+        if (timeoutId) {
+          clearTimeout(timeoutId);
+        }
+        const newTimeoutId = setTimeout(() => {
+          setStatus('');
+        }, 1000);
+        setTimeoutId(newTimeoutId);
+      })
+      .catch(error => {
+        console.log(error.response.data);
+        setStatus('Error sending email');
+        setStatusStyle(styles.statusError)
+      })
+  };
   const headline = useScreenSize({
     small: 'headlineSmall',
     medium: 'headlineMedium',
     large: 'headlineLarge'
   });
-
   const body = useScreenSize({
     small: 'bodySmall',
     medium: 'bodyMedium',
     large: 'bodyLarge'
   });
-
   const label = useScreenSize({
     small: 'labelSmall',
     medium: 'labelMedium',
@@ -21,21 +49,39 @@ const ContactForm = () => {
   });
 
   return (
-    <form className={styles.contactForm}>
-      <h2 className={headline}>Contáctenos</h2>
-      <input className={`${styles.input} ${body}`} type='text' placeholder={"Nombre"} />
-      <input className={`${styles.input} ${body}`} type='email' placeholder={"Email"} />
-      <input className={`${styles.input} ${body}`} placeholder={"Título"} />
-      <textarea
-        className={`${styles.text} ${body}`}
-        type='text'
-        rows={4}
-        placeholder={"Mensaje"}
-      />
-      <button
-        className={`botonBorder ${label}`}
-        type='submit'>Enviar Consulta</button>
-    </form>
+    <div className={styles.fromContainer}>
+      <form className={styles.contactForm} onSubmit={handleSubmit}>
+        <h2 className={headline}>Contáctenos</h2>
+        <div className={styles.formGroup}>
+          <input id='name' className={`${styles.input} ${body}`} type='text' placeholder={" "} value={name} onChange={(event) => setName(event.target.value)} required/>
+          <label htmlFor="name" className={styles.formLabel+' '+ body}>Nombre</label>
+        </div>
+        <div className={styles.formGroup}>
+          <input id='mail' className={`${styles.input} ${body}`} type='email' placeholder={" "} value={email} onChange={(event) => setEmail(event.target.value)} required/>
+          <label htmlFor="mail" className={styles.formLabel+' '+ body}>Email</label>
+        </div>
+        <div className={styles.formGroup}>
+          <textarea
+            className={`${styles.text} ${body}`}
+            id='text'
+            type='text'
+            rows={4}
+            placeholder={" "}
+            value={message}
+            onChange={(event) => setMessage(event.target.value)} 
+            required
+            />
+          <label htmlFor="text" className={styles.formLabel+' '+ body}>Mensaje</label>
+        </div>
+        <div className={styles.buttonContainer}>
+          <button
+            className={`botonBorder ${label}`}
+            type='submit'>Enviar Consulta
+          </button>
+          {status && <p className={statusStyle +' '+body}>{status}</p>}
+        </div>
+      </form>
+    </div>
   )
 }
 
