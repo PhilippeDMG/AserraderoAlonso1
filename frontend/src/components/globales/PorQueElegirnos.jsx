d15636bfa26b52448aaca29f38b01f4e88d679a5
000Lucifer <gabrielpaez0000@gmail.com>
2023-06-03 03:13:29 -0300

    retoque de por que elegirnos

--- frontend/src/components/globales/PorQueElegirnos.jsx
+++ frontend/src/components/globales/PorQueElegirnos.jsx
@@ -15,6 +15,13 @@ const PorQueElegirnos = () => {
       large: 'headlineLarge'
     }
   )
+  let titulo = useScreenSize(
+    {
+      small: 'titleSmall',
+      medium: 'titleMedium',
+      large: 'titleLarge'
+    }
+  )
   return (
     <div className={style.porQueElegirnos}>
       <div className={style.textoArriba}>
@@ -28,25 +35,37 @@ const PorQueElegirnos = () => {
           <img
             className={style.accountCircleIcon}
             alt=''
-            src='/account-circle21.svg'
+            src='/health-and-safety.svg'
           />
-          <div className={body}>Seguridad</div>
+          <div className={style.contenedor}>
+            <div className={titulo}>Seguridad</div>
+            <div className={body}>Protección confiable para pallets y tablas: Seguridad garantizada en nuestro tratamiento fitosanitario de aserradero.</div>
+          </div>
         </div>
         <div className={style.calidad}>
           <img className={style.accountCircleIcon} alt='' src='/eco.svg' />
-          <div className={body}>Experiencia</div>
+          <div className={style.contenedor}>
+            <div className={titulo}>Experiencia</div>
+            <div className={body}>Experiencia en cada paso: Nuestro aserradero brinda conocimientos sólidos en tratamientos fitosanitarios para pallets y tablas.</div>
+          </div>
         </div>
         <div className={style.calidad}>
           <img
             className={style.accountCircleIcon}
             alt=''
-            src='/health-and-safety.svg'
+            src='/account-circle21.svg'
           />
-          <div className={body}>Confianza</div>
+          <div className={style.contenedor}>
+            <div className={titulo}>Confianza</div>
+            <div className={body}>Confíe en nuestro aserradero: Garantizamos tratamientos fitosanitarios confiables y de calidad para pallets y tablas.</div>
+          </div>
         </div>
         <div className={style.calidad}>
           <img className={style.accountCircleIcon} alt='' src='/apartment.svg' />
-          <div className={body}>Eficiencia</div>
+          <div className={style.contenedor}>
+            <div className={titulo}>Eficiencia</div>
+            <div className={body}>Maximice la eficiencia en su producción: Nuestro aserradero ofrece tratamientos fitosanitarios rápidos y eficientes para pallets y tablas.</div>
+          </div>
         </div>
       </div>
     </div>
