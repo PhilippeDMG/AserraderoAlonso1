6a28e0acaf9e01551197b361ce7e5ae921580554
000Lucifer <gabrielpaez0000@gmail.com>
2023-07-17 15:27:52 -0300

    cambie mapa, favicon con .icon, hago pull

--- frontend/src/components/globales/Direccion.jsx
+++ frontend/src/components/globales/Direccion.jsx
@@ -2,7 +2,7 @@ import style from "./Direccion.module.css"
 const MapComponent = () => {
   return (
     <iframe
-      src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d54538.67864354442!2d-58.0535699!3d-31.3129362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95adebccd0789a4b%3A0x3a12cebd728c37e7!2sAserradero%20Alonso!5e0!3m2!1ses!2sar!4v1687584218181!5m2!1ses!2sar'
+      src="https://storage.googleapis.com/maps-solutions-g4shstlsrr/locator-plus/a4sm/locator-plus.html"
       width='100%'
       height='100%'
       allowFullScreen=''
